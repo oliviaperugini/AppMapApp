cc064f51ff6f461095529e7326873eea0fcdef99
zachfreed <freedzj@appstate.edu>
2019-02-27 17:06:56 -0500

    Added a Homepage.xml file and a Homepage.java class.

---
+++ app/src/main/java/com/example/practicemapapp/Homepage.java
@@ -0,0 +1,12 @@
+package com.example.practicemapapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Homepage extends Activity{
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.homepage);
+    }
+}
