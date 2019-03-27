0ff62c6069910327f5a0e3a646983981fb5d29f4
oliviaperugini <oliviaperugini@gmail.com>
2019-03-27 15:21:43 -0400

    Added MapMarkers class.

--- app/src/main/java/com/example/practicemapapp/MainActivity.java
+++ app/src/main/java/com/example/practicemapapp/MainActivity.java
@@ -1,9 +1,15 @@
 package com.example.practicemapapp;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.content.Context;
 import android.preference.PreferenceManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
 import org.osmdroid.views.MapView;
@@ -11,9 +17,15 @@ import org.osmdroid.config.Configuration;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.api.IMapController;
 
+
 public class MainActivity extends Activity {
-    MapView map = null;
-    @Override public void onCreate(Bundle savedInstanceState) {
+    MapView map;
+    IMapController mapController;
+
+    LocationManager locationManager;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         // IMPORTANT: handle permissions before map is created.
@@ -25,26 +37,20 @@ public class MainActivity extends Activity {
         // OSM Map Initialize
         // important! set your user agent to prevent getting banned from the osm servers
         //Configuration.getInstance().setUserAgentValue(BuildConfig.APPLICATION_ID);
-        map = (MapView) findViewById(R.id.map);
+        map = findViewById(R.id.map);
         map.setTileSource(TileSourceFactory.MAPNIK);
-
-        IMapController mapController = map.getController();
-        startMapView(mapController);
-        //mapController.setZoom(17);
-        //GeoPoint startPoint = new GeoPoint(36.214201, -81.679850);
-        //mapController.setCenter(startPoint);
-
-
-
-        // Setup MapView
-        //setupMapView();
-    }
-
-    public void startMapView(IMapController mc) {
-        mc.setZoom(17);
+        map.setMultiTouchControls(true);
+        mapController = map.getController();
+        mapController.setZoom(17);
         GeoPoint startPoint = new GeoPoint(36.214201, -81.679850);
-        mc.setCenter(startPoint);
+        mapController.setCenter(startPoint);
+
+        //
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+
     }
+
 
 
 }
